32d8121a745c6f84377fbf0e609903e0976123b2
crystalbae <baecry@gmail.com>
2017-03-21 14:16:48 +0900

    facebook login success

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '403383773360454', '4da86e2165b1b310dd21608722f4807d'
+  provider :facebook, '403383773360454
+', '4da86e2165b1b310dd21608722f4807d'
 end
